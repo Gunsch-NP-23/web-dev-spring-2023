ba0eb7255705412af611d0266ac85e4f49c483c3
Frank Gunsch <specificlaptop777@gmail.com>
2023-02-27 08:45:45 -0500

    Fix weird escape character bug?

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// Hacky fix not sure why it would default to escape characters being broken! Weird!
+import HomeView from `C:\\Users\\DELL\\Desktop\\CPS493\\Github_Repos\\web-dev-spring-2023\\client\\src\\views\\HomeView.vue`
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,7 +16,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('..\\views\\AboutView.vue')
     }
   ]
 })
